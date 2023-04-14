2e8bfde3235822081850dd748581850be60d919a
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-04-14 18:26:22 +0900

    feat : UserRepository 에 findByAlias 추가

--- src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/repository/UserRepository.java
+++ src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/repository/UserRepository.java
@@ -3,5 +3,8 @@ package io.github.tkdlqh2.multiplication_msa.multiplication.repository;
 import io.github.tkdlqh2.multiplication_msa.multiplication.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+	Optional<User> findByAlias(String alias);
 }
