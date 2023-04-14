c8417c05725f8f1e631dea7daf44b54f1406ad65
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-04-14 18:37:46 +0900

    fix : findAllByUser return 값 List 에서 page 로 변경

--- src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/repository/MultiplicationResultAttemptRepository.java
+++ src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/repository/MultiplicationResultAttemptRepository.java
@@ -2,11 +2,10 @@ package io.github.tkdlqh2.multiplication_msa.multiplication.repository;
 
 import io.github.tkdlqh2.multiplication_msa.multiplication.domain.MultiplicationResultAttempt;
 import io.github.tkdlqh2.multiplication_msa.multiplication.domain.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MultiplicationResultAttemptRepository extends JpaRepository<MultiplicationResultAttempt,Long> {
-	List<MultiplicationResultAttempt> findAllByUser(User user, Pageable pageable);
+	Page<MultiplicationResultAttempt> findAllByUser(User user, Pageable pageable);
 }
