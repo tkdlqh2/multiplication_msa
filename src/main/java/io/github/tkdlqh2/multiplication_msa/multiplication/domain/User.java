21f54f4f38e2edd42a47b08ead4812887b5b0fcf
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-04-14 16:28:43 +0900

    feat : User class 추가

---
+++ src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/domain/User.java
@@ -0,0 +1,23 @@
+package io.github.tkdlqh2.multiplication_msa.multiplication.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+/**
+ * 사용자 정보를 저장하는 클래스
+ */
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class User {
+
+	private final String alias;
+
+	// JSON (역)직렬화를 위한 빈 생성자
+	protected User() {
+		alias = null;
+	}
+}
