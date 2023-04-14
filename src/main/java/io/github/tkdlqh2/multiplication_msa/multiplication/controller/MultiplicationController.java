e1e1371d013ac2f6e15e1068d2bdba8e5942f36c
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-04-14 17:05:32 +0900

    fix : URL s 빠진 것 추가

--- src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/controller/MultiplicationController.java
+++ src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/controller/MultiplicationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/multiplication")
+@RequestMapping("/multiplications")
 @RequiredArgsConstructor
 public class MultiplicationController {
 
