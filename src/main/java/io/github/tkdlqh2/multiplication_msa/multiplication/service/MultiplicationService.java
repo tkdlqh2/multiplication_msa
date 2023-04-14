058812653c5023d412e7660d3cdbfb5d11780e4f
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-04-14 18:24:16 +0900

    feat : check attempt 메소드에 repository 저장 추가 및 getStatsForUser 메소드 추가

--- src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/service/MultiplicationService.java
+++ src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/service/MultiplicationService.java
@@ -3,6 +3,8 @@ package io.github.tkdlqh2.multiplication_msa.multiplication.service;
 import io.github.tkdlqh2.multiplication_msa.multiplication.domain.Multiplication;
 import io.github.tkdlqh2.multiplication_msa.multiplication.domain.MultiplicationResultAttempt;
 
+import java.util.List;
+
 public interface MultiplicationService {
 
 	/**
@@ -17,4 +19,12 @@ public interface MultiplicationService {
 	 * @return 곱셈 계산 결과가 맞으면 true, 아니면 false
 	 */
 	boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
+
+	/**
+	 * 해당 사용자의 통계 정보를 조회한다.
+	 *
+	 * @param userAlias 는 해당 사용자의 닉네임
+	 * @return 해당 사용자가 전에 제출한 답안 객체 {@link MultiplicationResultAttempt}의 리스트
+	 */
+	List<MultiplicationResultAttempt> getStatsForUser(final String userAlias);
 }
--- src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/service/MultiplicationServiceImpl.java
+++ src/main/java/io/github/tkdlqh2/multiplication_msa/multiplication/service/MultiplicationServiceImpl.java
@@ -2,14 +2,30 @@ package io.github.tkdlqh2.multiplication_msa.multiplication.service;
 
 import io.github.tkdlqh2.multiplication_msa.multiplication.domain.Multiplication;
 import io.github.tkdlqh2.multiplication_msa.multiplication.domain.MultiplicationResultAttempt;
+import io.github.tkdlqh2.multiplication_msa.multiplication.domain.User;
+import io.github.tkdlqh2.multiplication_msa.multiplication.exception.MultiplicationException;
+import io.github.tkdlqh2.multiplication_msa.multiplication.repository.MultiplicationResultAttemptRepository;
+import io.github.tkdlqh2.multiplication_msa.multiplication.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static io.github.tkdlqh2.multiplication_msa.multiplication.exception.MultiplicationErrorCode.ALREADY_CHECKED;
 
 @Service
 @RequiredArgsConstructor
 public class MultiplicationServiceImpl implements MultiplicationService{
 
 	private final RandomGeneratorService randomGeneratorService;
+	private final MultiplicationResultAttemptRepository attemptRepository;
+	private final UserRepository userRepository;
 
 	@Override
 	public Multiplication createRandomMultiplication() {
@@ -19,10 +35,44 @@ public class MultiplicationServiceImpl implements MultiplicationService{
 		return new Multiplication(factorA,factorB);
 	}
 
+	@Transactional
 	@Override
-	public boolean checkAttempt(final MultiplicationResultAttempt resultAttempt) {
-		return resultAttempt.getResultAttempt() ==
-				resultAttempt.getMultiplication().getFactorA() *
-						resultAttempt.getMultiplication().getFactorB();
+	public boolean checkAttempt(final MultiplicationResultAttempt attempt) {
+		// 해당 닉네임의 사용자가 존재하는지 확인
+		Optional<User> user = userRepository.findByAlias(attempt.getUser().getAlias());
+
+		// 조작된 답안을 방지
+		if(attempt.isCorrect()){
+			throw new MultiplicationException(ALREADY_CHECKED);
+		}
+
+		// 답안을 채점
+		boolean isCorrect = attempt.getResultAttempt() ==
+				attempt.getMultiplication().getFactorA() *
+						attempt.getMultiplication().getFactorB();
+
+		MultiplicationResultAttempt checkedAttempt = new MultiplicationResultAttempt(
+				user.orElse(attempt.getUser()),
+				attempt.getMultiplication(),
+				attempt.getResultAttempt(),
+				isCorrect
+		);
+
+		// 답안을 저장
+		attemptRepository.save(checkedAttempt);
+		return isCorrect;
+	}
+
+	@Override
+	public List<MultiplicationResultAttempt> getStatsForUser(String userAlias) {
+		Optional<User> optionalUser = userRepository.findByAlias(userAlias);
+
+		if(optionalUser.isPresent()){
+			User user = optionalUser.get();
+			Pageable pageable = PageRequest.of(0,5, Sort.by(Sort.Order.desc("Id")));
+			return attemptRepository.findAllByUser(user,pageable);
+		}
+
+		return Collections.emptyList();
 	}
 }
