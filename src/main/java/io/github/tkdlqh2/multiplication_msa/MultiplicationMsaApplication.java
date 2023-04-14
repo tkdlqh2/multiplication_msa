fbc549e72800be291e8eea5f42aacda019424768
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-04-14 15:34:57 +0900

    Initial commit

---
+++ src/main/java/io/github/tkdlqh2/multiplication_msa/MultiplicationMsaApplication.java
@@ -0,0 +1,13 @@
+package io.github.tkdlqh2.multiplication_msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplicationMsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplicationMsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tkdlqh2/multiplication_msa/MultiplicationMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tkdlqh2.multiplication_msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplicationMsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
